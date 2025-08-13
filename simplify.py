c9fcef2070821cbd83ea7a35815021c48963a5ab
harjeet <hkkalsi1@gmail.com>
2025-08-13 19:43:20 +0100

    initial working of trafilatura and selenium

--- simplify.py
+++ simplify.py
@@ -1,5 +1,7 @@
 from openai import OpenAI
 from dotenv import load_dotenv
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
 load_dotenv()
 
@@ -40,3 +42,24 @@ def simplify_from_urls(url):
         return response.choices[0].message.content.strip()
     except Exception as e:
         return f"⚠️ Error: {e}" 
+
+def extract_text_from_url(url): 
+    url = "https://wellcomeopenresearch.org/articles/10-318"
+    html = urlopen(url).read()
+    soup = BeautifulSoup(html, features="html.parser")
+
+    # kill all script and style elements
+    for script in soup(["script", "style"]):
+        script.extract()    # rip it out
+
+    # get text
+    text = soup.get_text()
+
+    # break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in text.splitlines())
+    # break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # drop blank lines
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+
+    print(text)
